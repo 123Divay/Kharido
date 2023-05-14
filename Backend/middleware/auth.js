4422fb194e821b93daa1dc337968bc1347b698e6
Divay <Divay@example.com>
2023-05-14 11:18:30 -0700

    fifth commit

--- Backend/middleware/auth.js
+++ Backend/middleware/auth.js
@@ -1,11 +1,32 @@
-const ErrorHandler = require("../utils/errorhandler");
+const ErrorHander = require("../utils/errorhandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
 const jwt = require("jsonwebtoken");
-exports.isAuthenticatedUser = catchAsyncErrors(async(req,res,next)=>{
-  const {token} = req.cookies;
+const User = require("../models/userModel");
+
+exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
+  const { token } = req.cookies;
+
   if (!token) {
-    return next(new ErrorHandler("Please login to acces the resource",401));
+    return next(new ErrorHander("Please Login to access this resource", 401));
   }
-  const decodeData = jwt.verify(token,process.env.JWT_SECRET);
-  
+
+  const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+
+  req.user = await User.findById(decodedData.id);
+  next();
 });
+
+exports.authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorHander(
+          `Role: ${req.user.role} is not allowed to access this resouce `,
+          403
+        )
+      );
+    }
+
+    next();
+  };
+};
