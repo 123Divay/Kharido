5bc29016a1b9f89f43a864de6d3560b991bef4e1
Divay <Divay@example.com>
2023-05-19 11:08:50 -0700

    eight commit

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -89,3 +89,25 @@ exports.resetPassword = catchAsyncErrors(async(req,res,next)=> {
   await user.save();
   sendToken(user,200,res);
 });
+
+exports.getUserDetails = catchAsyncErrors(async(req,res,next)=> {
+  const user = await User.findById(req.user.id);
+  res.status(200).json({
+    success:true,
+    user,
+  });
+});
+
+exports.updatePassword = catchAsyncErrors(async(req,res,next)=> {
+  const user = await User.findById(req.user.id).select("+password");
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old Password is incorrect",400));
+  }
+  if (req.body.newPassword !== req.body.confirmPassword) {
+      return next(new ErrorHandler("Password does not match",400));
+  }
+  user.password = req.body.newPassword;
+  await user.save();
+  sendToken(user,200,res);
+});
--- Backend/middleware/error.js
+++ Backend/middleware/error.js
@@ -8,7 +8,18 @@ module.exports = (err,req,res,next) => {
     const message = `Resource not found. Invalid : ${err.path}`;
     err = new ErrorHandler(message,400);
   }
-
+  if (err.code === 11000) {
+    const message = `Duplicate ${Object.keys(err.keyValue)} entered`;
+    err = new ErrorHandler(message,400);
+  }
+  if (err.name === "JsonWebTokenError") {
+    const message = `Json Web Token is invalid, try again`;
+    err = new ErrorHandler(message,400);
+  }
+  if (err.name === "TokenExpireError") {
+    const message = `Json Web Token is expired, try again`;
+    err = new ErrorHandler(message,400);
+  }
   res.status(err.statusCode).json({
     success:false,
     message:err.message
