c022dd28c09d1e697e56c7e9d42234cb90fee9e7
Divay <Divay@example.com>
2023-05-28 17:24:17 -0700

    seventh commit

--- Backend/routes/userRoute.js
+++ Backend/routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const {registerUser,loginUser,logoutUser,forgotPassword,resetPassword,getUserDetails,updatePassword} = require("../controllers/userController");
+const {registerUser,loginUser,logoutUser,forgotPassword,resetPassword,getUserDetails,updatePassword,updateProfile,getAllUser,getSingleUser} = require("../controllers/userController");
 const {isAuthenticatedUser,authorizeRoles} = require("../middleware/auth");
 const router = express.Router();
 router.route("/register").post(registerUser);
@@ -9,4 +9,7 @@ router.route("/password/reset/:token").put(resetPassword);
 router.route("/logout").get(logoutUser);
 router.route("/me").get(isAuthenticatedUser,getUserDetails);
 router.route("/password/update").put(isAuthenticatedUser,updatePassword);
+router.route("/me/update").put(isAuthenticatedUser,updateProfile);
+router.route("/admin/users").get(isAuthenticatedUser,authorizeRoles("admin"),getAllUser);
+router.route("/admin/user/:id").get(isAuthenticatedUser,authorizeRoles("admin"),getSingleUser);
 module.exports = router;
